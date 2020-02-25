f97f7010844d0d8ce1235c05815532c6805b8bfc
Lucho <luchoster@gmail.com>
2020-02-24 17:34:26 -0800

    Adding the env token to the block content to see if that helps to pull the data

--- src/client-config.js
+++ src/client-config.js
@@ -2,5 +2,6 @@ module.exports = {
   sanity: {
     projectId: process.env.SANITY_ID || '<#< sanity.projectId >#>',
     dataset: process.env.SANITY_DATASET || 'production',
+    token: process.env.SANITY_TOKEN,
   },
 }
